459b2b83e9a4e7044253b6eaeef5faa38fa2239c
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-04-12 15:52:52 +0200

    feat: header

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import Header from './components/Header'
 import Home from './pages/Home'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <Home />
   </React.StrictMode>
 );
