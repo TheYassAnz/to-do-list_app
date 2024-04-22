b3cb044c0f394d7685eb98f3a79b2bd7ca2495c1
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-04-22 16:46:16 +0200

    refractor: implement handlers in the component

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,9 +1,42 @@
-export default function Task({ value, onChange, uniqueIdentifier }) {
+import axios from "axios";
+export default function Task({ tasks, setTasks, selectedTask }) {
+    function archiveTask() {
+        const newList = tasks.map((task) => {
+            if (task.id === selectedTask.id) {
+                const updatedTask = {
+                    ...task,
+                    archived: !task.archived,
+                };
+                return updatedTask;
+            }
+            return task;
+        });
+        setTasks(newList);
+        const updateTask = async (task) => {
+            axios.put(`http://localhost:8000/api/tasks/${task.id}`, { archived: !task.archived })
+                .then((response) => console.log(response))
+                .catch((error) => console.log(error))
+        }
+        updateTask(selectedTask)
+    }
+
+    function deleteTask() {
+        setTasks(oldTasks => {
+            return oldTasks.filter(task => task.id !== selectedTask.id)
+        })
+        const deleteData = async (task) => {
+            axios.delete(`http://localhost:8000/api/tasks/${task.id}`)
+                .then((response) => console.log(response))
+                .catch((error) => console.log(error))
+        }
+        deleteData(selectedTask);
+    }
     return (
 
         <li>
-            <input type="checkbox" name="task" id={uniqueIdentifier} onChange={onChange} />
-            <label for={uniqueIdentifier}>{value}</label>
+            <button onClick={deleteTask}>-</button>
+            <input type="checkbox" name="task" id={selectedTask.id} onChange={archiveTask} />
+            <label for={selectedTask.id}>{selectedTask.name}</label>
         </li>
     )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,56 +5,32 @@ import axios from 'axios';
 
 export default function Home() {
     const [tasks, setTasks] = useState([]);
-
     useEffect(() => {
         const fetchData = async () => {
-            //   setLoading(true);
             try {
                 const { data: response } = await axios.get('http://localhost:8000/api/tasks');
                 setTasks(response);
             } catch (error) {
                 console.error(error.message);
             }
-            // setLoading(false);
         }
 
         fetchData();
     }, []);
-
-    function handleChange(checkedTask) {
-        const newList = tasks.map((task) => {
-            if (task.id === checkedTask.id) {
-                const updatedTask = {
-                    ...task,
-                    archived: !task.archived,
-                };
-                return updatedTask;
-            }
-            return task;
-        });
-        setTasks(newList);
-        const updateTask = async (task) => {
-            axios.put(`http://localhost:8000/api/tasks/${task.id}`, { archived: !task.archived })
-                .then((response) => console.log(response))
-                .catch((error) => console.log(error))
-        }
-        updateTask(checkedTask)
-
-    }
     return (
         <>
             <h2>Welcome to the Home Page 🙋</h2>
             <h3>Current Tasks</h3>
             <ul style={{ listStyle: 'none' }}>
                 {tasks.map((task, index) => (
-                    !task.archived && <Task key={index} uniqueIdentifier={'task' + task.id} value={task.name} onChange={e => handleChange(task)} />
+                    !task.archived && <Task key={index} selectedTask={task} tasks={tasks} setTasks={setTasks} />
                 ))}
             </ul>
             <details>
                 <summary style={{ fontSize: 'large' }}>Archived Tasks</summary>
                 <ul style={{ listStyle: 'none' }}>
                     {tasks.map((task, index) => (
-                        task.archived && <Task key={index} uniqueIdentifier={'task' + task.id} value={task.name} onChange={e => handleChange(task)} />
+                        task.archived && <Task key={index} selectedTask={task} tasks={tasks} setTasks={setTasks} />
                     ))}
                 </ul>
             </details><br />
