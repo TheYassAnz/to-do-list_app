a3d46f656b7ee5e968f78707d3ac8ffd19bb0972
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-04-22 16:58:34 +0200

    feat: implement react-router-dom@6.22.3

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,12 @@
+import { Outlet } from "react-router-dom";
+
 export default function Header() {
     return (
-        <header>
-            <h1>To-Do-List</h1>
-        </header>
+        <>
+            <header>
+                <h1>To-Do-List</h1>
+            </header>
+            <Outlet />
+        </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header'
 import Home from './pages/Home'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { redirect } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Header />,
+    children: [
+      {
+        path: '/home',
+        element: <Home />,
+      },
+    ]
+  },
+
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
-    <Home />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
