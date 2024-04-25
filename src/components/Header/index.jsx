d7bbaaf55efa395bfb09e111aae826e04044eeeb
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-04-25 16:07:14 +0200

    feat: Link ['/login', '/register']

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Header() {
     return (
         <>
             <header>
                 <h1>To-Do-List</h1>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
             </header>
             <Outlet />
         </>
